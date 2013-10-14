a583bdfac078e5fcb68334bf342fa9f85c740c68
Arkady Pevzner <arkady.pevzner@gmail.com>
2013-10-14 16:43:47 -0700

    Added functionality for simple objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,22 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+  
+	if(typeof(obj) === 'string') {
+		return "\""+obj+"\"";
+	}
+	else if(typeof(obj) === 'number' || typeof(obj) === 'boolean') {
+		return obj.toString();
+	}
+	else if(typeof(obj) === 'undefined' || typeof(obj) === 'function') {
+		return undefined;
+	}
+	else if(obj === null) {
+		return 'null';
+	}
+	else if(Array.isArray(obj)) {
+	}
+	else { //obj is an Object with keys and values
+	}
+
 };
